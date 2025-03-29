41212974d27aca0e70520424f75d12b70e69b66e
gcordner <geoffcordner@gmail.com>
2025-03-29 19:06:46 +0000

    webpack, build stuff, functions.php to enqueue it all

---
+++ functions.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Former Model Theme Functions
+ *
+ * @package Former_Model
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+/**
+ * Former Model Theme Setup
+ */
+function former_model_setup() {
+	add_theme_support( 'block-editor-style' );
+	add_theme_support( 'editor-styles' );
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+
+	register_nav_menus(
+		array(
+			'primary' => __( 'Primary Menu', 'former-model' ),
+		)
+	);
+}
+add_action( 'after_setup_theme', 'former_model_setup' );
+
+/**
+ * Enqueue theme CSS/JS compiled via Webpack.
+ */
+function former_model_enqueue_assets() {
+	// CSS: css/build/theme.min.[hash].css.
+	$css_file_path = glob( get_template_directory() . '/css/build/theme.min.*.css' );
+	if ( ! empty( $css_file_path ) ) {
+		$css_file_uri = get_template_directory_uri() . '/css/build/' . basename( $css_file_path[0] );
+		wp_enqueue_style( 'former_model_style', $css_file_uri ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
+	}
+
+	// JS: js/build/main.min.[hash].js.
+	$js_file_path = glob( get_template_directory() . '/js/build/main.min.*.js' );
+	if ( ! empty( $js_file_path ) ) {
+		$js_file_uri = get_template_directory_uri() . '/js/build/' . basename( $js_file_path[0] );
+		wp_enqueue_script( 'former_model_script', $js_file_uri, array(), null, true ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
+	}
+}
+add_action( 'wp_enqueue_scripts', 'former_model_enqueue_assets', 20 );
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: ['./js/shrink-header.js', './css/src/main.scss']
+    },
+    output: {
+        path: path.resolve(__dirname),
+        filename: 'js/build/main.min.[fullhash].js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(s[ac]ss|css)$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [
+                'js/build/*',
+                'css/build/*'
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/build/theme.min.[fullhash].css'
+        })
+    ],
+    optimization: {
+        minimizer: [
+            '...', // JS minimizer
+            new CssMinimizerPlugin()
+        ]
+    }
+};
